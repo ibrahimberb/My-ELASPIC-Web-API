8fdb4cc848ec8ac24f12a211341e5297deb9c060
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-13 13:47:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import pathlib
 
-COMPUTATION_TIME_ALLOWED = 10  # in minutes.
+COMPUTATION_TIME_ALLOWED = 3  # in seconds.
 # Paths
 DRIVER_PATH = r"C:\webdrivers\geckodriver.exe"
 # r"C:\Users\ibrah\Desktop\Spaceship\ELASPIC_cancer_data_smaller_chunks\ELASPIC_Input\BRCA_10\SNV_BRCA_Chunk_22_0_test.txt"
@@ -9,6 +9,8 @@ DRIVER_PATH = r"C:\webdrivers\geckodriver.exe"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\mutations_input_test_10.txt"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\mutation_all_error_test.txt"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_0_test.txt"
-UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_1_test.txt"
+# UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_1_test.txt"
+UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_7_test.txt"
 DOWNLOAD_FOLDER_PATH = pathlib.Path().resolve() / 'Firefox_download'
 RECORDS_FOLDER_PATH = "Records_Test"
+ELASPIC_MANY_URL = 'http://elaspic.kimlab.org/many/'
