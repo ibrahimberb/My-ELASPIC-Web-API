7f0e01491d5031f3621fc1006a6564c337acb03f
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-14 01:07:41 +0300

    Delete basics_3.py

--- basics_3.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.DataFrame({'A': [1, 2, 3],
-                     'B': [4, 5, 6],
-                     'C': [7, 8, 9]}).set_index('A')
-# print(data)
-print(data.loc[2, 'C'])
