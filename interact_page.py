93c443901052e816863ccfb50ba1a6e8d8bdd4ea
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-14 03:18:56 +0300

    Update interact_page.py

--- interact_page.py
+++ interact_page.py
@@ -1,11 +1,10 @@
 from selenium.common.exceptions import NoSuchElementException
 import logging
-from driver_conf import initialize_driver
 from selenium.webdriver.support.ui import Select
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-STATUS_TYPES = ['done', 'error']
+PAGE_POST_STATUSES = ['done', 'error', 'running']
 
 
 def check_exists_by_id(driver, element_id):
@@ -24,10 +23,8 @@ def check_exists_by_xpath(driver, element_xpath):
     return True
 
 
-def get_post_info(driver, status_type):
-    assert status_type in STATUS_TYPES
-
-    info_prot_mut_pairs = []
+def get_post_info(driver):
+    post_info = {f'mutations_{status}': [] for status in PAGE_POST_STATUSES}
 
     select = Select(driver.find_element_by_class_name('pagesize'))
     select.select_by_value('all')
@@ -35,19 +32,14 @@ def get_post_info(driver, status_type):
     rows = table_id.find_elements_by_tag_name("tr")[1:]
 
     for row in rows:
-        protein_mutation = row.get_attribute('data-pnt')
+        protein_mutation = row.get_attribute('data-pnt').split('_')[0]  # P01116.G12V_13360419 → P01116.G12V
 
-        attr = row.get_attribute("class").split(' ')[0]
+        status = row.get_attribute("class").split(' ')[0]
+        post_info[f"mutations_{status}"].append(protein_mutation)
+        logging.debug(f"{protein_mutation} \t {status}")
 
-        if attr == status_type:
-            info_prot_mut_pairs.append(protein_mutation)
+    for status in PAGE_POST_STATUSES:
+        post_info[f'num_mutations_{status}'] = len(post_info[f'mutations_{status}'])
 
-        print(f"{protein_mutation} \t {attr}")
-
-    post_info = {'elements': info_prot_mut_pairs,
-                 'num_elements': len(info_prot_mut_pairs)}
-
-    print('ELEMENTS: {}'.format(post_info['elements']))
-    print('NUM_ELEMENTS: {}'.format(post_info['num_elements']))
-    print('= = = = = = = = = = = = = = = = = = ')
+    logging.debug('post_info: {}'.format(post_info))
     return post_info
