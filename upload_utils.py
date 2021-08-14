db8abc98b97fbf995c8eabcd738d7a52da18d88d
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-14 10:04:05 +0300

    Update upload_utils.py

--- upload_utils.py
+++ upload_utils.py
@@ -1,7 +1,7 @@
 from utils import is_valid_file
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 
 def upload_file(driver, upload_file_path):
@@ -9,4 +9,4 @@ def upload_file(driver, upload_file_path):
     upload_file_button = driver.find_element_by_id("pfile")
     assert is_valid_file(upload_file_path), 'check your file!'
     upload_file_button.send_keys(upload_file_path)
-    logging.info('Uploaded file: {}'.format(upload_file_path))
+    logging.debug('Uploaded file: {}'.format(upload_file_path))
