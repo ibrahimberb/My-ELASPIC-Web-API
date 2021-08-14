2a24a53e7acbd3fe8c063c605aa720e5f9af1168
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-14 10:04:10 +0300

    Update basics.py

--- test_scripts/basics.py
+++ test_scripts/basics.py
@@ -1,24 +1,10 @@
-from tqdm import tqdm
-import time
-import random
-
-i = 0
-a = 0
-
-# pbar = tqdm(total=100)
-# for i in range(10):
-#     sleep(0.1)
-#     pbar.update(10)
-# pbar.close()
-
-pbar = tqdm(total=100, desc='DEBUG_DELAY')
-while i < 50 or a != 3:
-    time.sleep(0.03)
-    a = random.randint(0, 10)
-    pbar.update(1)
-    i = i + 1
-    # if a == 3:
-    #     print(f'a={a}')
-# pbar.update(pbar.g)
-pbar.clear()
-pbar.close()
+# from record import Record
+# from chunk import Chunk
+# from config import RECORDS_FOLDER_PATH
+#
+# chunk_filepath = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\OV_10_test\*"
+#
+# # maybe file name is the same as with object name
+# record = Record(records_folder_path=RECORDS_FOLDER_PATH, Chunk())
+#
+# make_chunk_from_record
