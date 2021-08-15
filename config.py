c9fe32e3a109799fb0cd3a386e37901e4a0c9c1b
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-15 12:18:06 +0300

    Update config.py
    Added UNEXPECTED_FAILED_PATH

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
 import pathlib
 
-test_mode = True
+test_mode = False
 
 if test_mode:
     RECORDS_FOLDER_PATH = "test_files/Records_Test"
     ELASPIC_RESULTS_FOLDER_PATH = "test_files/ELASPIC_Results_TEST"
     UPLOAD_FAILED_PATH = r"upload_failed_records_TEST.txt"
+    UNEXPECTED_FAILED_PATH = r"unexpected_failed_records_TEST.txt"
     INPUT_FILES_PATH = "-----"
 
+# ACTUAL RUN
 else:
     RECORDS_FOLDER_PATH = "Records"
     ELASPIC_RESULTS_FOLDER_PATH = "Elaspic_Results"
-    UPLOAD_FAILED_PATH = "upload_failed_records.txt"
+    UPLOAD_FAILED_PATH = "Upload_fails"
+    UNEXPECTED_FAILED_PATH = "Unexpected_fails"
     INPUT_FILES_PATH = "ELASPIC_Input"
 
 COMPUTATION_TIME_ALLOWED = 1  # 3 # 10  # in seconds.
