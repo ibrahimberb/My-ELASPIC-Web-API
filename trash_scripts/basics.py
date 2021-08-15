0aa986639cf1725febc620265654a7fa018efe84
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-15 20:24:50 +0300

    Update basics.py

--- trash_scripts/basics.py
+++ trash_scripts/basics.py
@@ -1,5 +1,11 @@
-from pathlib import Path
-import os
-
-print(type(os.getcwd()))
-print(type(Path(os.getcwd())))
+# import pandas as pd
+#
+# data = pd.read_csv("../Records/OV/record_OV_1.csv", index_col='FILE_PATH')
+#
+# print(data)
+#
+# print('--------------------------------------')
+#
+# data = data.drop('SNV_OV_Chunk_1_11.txt', axis='index')
+#
+# print(data)
