cc5c42f9148bee2b1a1a4dcaaabed95abc8e6e5c
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-15 03:25:23 +0300

    Update basics.py

--- test_scripts/basics.py
+++ test_scripts/basics.py
@@ -1,10 +1,5 @@
-# from record import Record
-# from chunk import Chunk
-# from config import RECORDS_FOLDER_PATH
-#
-# chunk_filepath = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\OV_10_test\*"
-#
-# # maybe file name is the same as with object name
-# record = Record(records_folder_path=RECORDS_FOLDER_PATH, Chunk())
-#
-# make_chunk_from_record
+from pathlib import Path
+import os
+
+print(type(os.getcwd()))
+print(type(Path(os.getcwd())))
