2be95d1ca6614d931809bc7c7ad58e4fbf5340c7
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-15 06:14:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,23 @@
 import pathlib
 
+test_mode = True
+
+if test_mode:
+    RECORDS_FOLDER_PATH = "Records_Test"
+    ELASPIC_RESULTS_FOLDER_PATH = "ELASPIC_Results_TEST"
+else:
+    RECORDS_FOLDER_PATH = "Records"
+    ELASPIC_RESULTS_FOLDER_PATH = "ELASPIC_Results"
+
 COMPUTATION_TIME_ALLOWED = 1  # 3 # 10  # in seconds.
+
 # Paths
 DRIVER_PATH = r"C:\webdrivers\geckodriver.exe"
+TEMP_DOWNLOAD_FOLDER_PATH = pathlib.Path().resolve() / 'Firefox_download'
+ELASPIC_MANY_URL = 'http://elaspic.kimlab.org/many/'
+UPLOAD_FAILED_PATH = r"upload_failed_records.txt"
+HEADLESS = True
+
 # r"C:\Users\ibrah\Desktop\Spaceship\ELASPIC_cancer_data_smaller_chunks\ELASPIC_Input\BRCA_10\SNV_BRCA_Chunk_22_0_test.txt"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\mutations_input_test.txt"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\mutation_input_test_40.txt"
@@ -10,9 +25,7 @@ DRIVER_PATH = r"C:\webdrivers\geckodriver.exe"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\mutation_all_error_test.txt"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_0_test.txt"
 # UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_1_test.txt"
-UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_7_test.txt"
-DOWNLOAD_FOLDER_PATH = pathlib.Path().resolve() / 'Firefox_download'
-RECORDS_FOLDER_PATH = "Records_Test"
-ELASPIC_MANY_URL = 'http://elaspic.kimlab.org/many/'
-UPLOAD_FAILED_PATH = r"upload_failed_records.txt"
-HEADLESS = True
+# UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_files_test\SNV_BRCA_Chunk_22_7_test.txt"
+
+
+# TODO records icin Folder creation... BRCA, OV.
