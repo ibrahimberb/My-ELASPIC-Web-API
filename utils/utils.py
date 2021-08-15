359c78cfe6a8c9ec105424150e3472bd4fbda2f2
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-15 12:18:27 +0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -2,16 +2,25 @@ import logging
 import os
 import time
 from tqdm import tqdm
-from config import UPLOAD_FAILED_PATH
+from config import UPLOAD_FAILED_PATH, UNEXPECTED_FAILED_PATH
 import glob
+from utils.organizer import parse_filename
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 
-def get_subchunk_files(chunk_path):
+def get_subchunk_files(subchunks_path, tcga=None, chunk_no=None):
+    if tcga is not None:
+        subchunks_path = os.path.join(os.getcwd(), subchunks_path, tcga, str(chunk_no), '*')
+    else:
+        subchunks_path = os.path.join(os.getcwd(), subchunks_path, '*')
+
+    logging.info(f"subchunks_path: {subchunks_path}")
     files = [file for file in
-             glob.glob(chunk_path)
-             if f'Chunk_{TODO}' in file]
+             glob.glob(subchunks_path)
+             if '.txt' in file]
+    # logging.info(f"FILES: {files}")
+    return files
 
 
 def wait(duration, desc=None):
@@ -42,17 +51,27 @@ def get_filename_from_path(filepath):
 
 
 def record_upload_failed(filename, upload_failed_path=UPLOAD_FAILED_PATH):
-    if not os.path.isfile(upload_failed_path):
-        logging.info(f"Creating upload fail record file {upload_failed_path}")
-        with open(upload_failed_path, 'w'): pass
+    record_bad_states(filename, "upload fail", upload_failed_path)
 
-    with open(upload_failed_path) as file:
+
+def record_unexpected_failed(filename, unexpected_failed_path=UNEXPECTED_FAILED_PATH):
+    record_bad_states(filename, "unexpected", unexpected_failed_path)
+
+
+def record_bad_states(filename, bad_state, bad_state_path=UPLOAD_FAILED_PATH):
+    tcga_code, _, _ = parse_filename(filename)
+    bad_state_path = os.path.join(bad_state_path, tcga_code + '.txt')
+    if not os.path.isfile(bad_state_path):
+        logging.info(f"Creating {bad_state} record file {bad_state_path}")
+        with open(bad_state_path, 'w'): pass
+
+    with open(bad_state_path) as file:
         lines = file.readlines()
         lines = [line.strip() for line in lines]
         if filename in lines:
-            logging.info(f"{filename} already recorded as failed.")
+            logging.info(f"{filename} already recorded as {bad_state}.")
             return
 
-    with open(upload_failed_path, 'a') as file:
+    with open(bad_state_path, 'a') as file:
         logging.info(f"Recording {filename} as failed ..")
         file.write(f"{filename}\n")
