9f978788b24391ffd9ff49c302be02b89bbaa2ef
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-15 03:25:00 +0300

    Update driver_conf.py

--- driver_conf.py
+++ driver_conf.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from config import DOWNLOAD_FOLDER_PATH, DRIVER_PATH
+from config import DOWNLOAD_FOLDER_PATH, DRIVER_PATH, HEADLESS
 
 
 def initialize_driver():
@@ -10,8 +10,11 @@ def initialize_driver():
     profile.set_preference("browser.download.dir", str(DOWNLOAD_FOLDER_PATH))
     profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/plain')  # type of file to download
 
+    options = webdriver.FirefoxOptions()
+    options.headless = HEADLESS
+
     # Set the driver
-    driver = webdriver.Firefox(executable_path=DRIVER_PATH, firefox_profile=profile)
+    driver = webdriver.Firefox(options=options, executable_path=DRIVER_PATH, firefox_profile=profile)
     driver.set_window_position(1024, 0)
     driver.set_window_size(1920 - 1024, 1040)
 
