bb0f86f21b91415aee66fbd893a74e67dc279ac7
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-15 03:24:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import pathlib
 
-COMPUTATION_TIME_ALLOWED = 1 # 3 # 10  # in seconds.
+COMPUTATION_TIME_ALLOWED = 1  # 3 # 10  # in seconds.
 # Paths
 DRIVER_PATH = r"C:\webdrivers\geckodriver.exe"
 # r"C:\Users\ibrah\Desktop\Spaceship\ELASPIC_cancer_data_smaller_chunks\ELASPIC_Input\BRCA_10\SNV_BRCA_Chunk_22_0_test.txt"
@@ -14,3 +14,5 @@ UPLOAD_FILE_PATH = r"C:\Users\ibrah\Documents\GitHub\My-ELASPIC-Web-API\input_fi
 DOWNLOAD_FOLDER_PATH = pathlib.Path().resolve() / 'Firefox_download'
 RECORDS_FOLDER_PATH = "Records_Test"
 ELASPIC_MANY_URL = 'http://elaspic.kimlab.org/many/'
+UPLOAD_FAILED_PATH = r"upload_failed_records.txt"
+HEADLESS = True
