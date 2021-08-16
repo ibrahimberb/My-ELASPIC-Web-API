42aeb9bb38826eab0941b3b8b7a776933b930065
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-16 19:55:15 +0300

    Update basics.py

--- trash_scripts/basics.py
+++ trash_scripts/basics.py
@@ -1,11 +1,10 @@
-# import pandas as pd
-#
-# data = pd.read_csv("../Records/OV/record_OV_1.csv", index_col='FILE_PATH')
-#
-# print(data)
-#
-# print('--------------------------------------')
-#
-# data = data.drop('SNV_OV_Chunk_1_11.txt', axis='index')
-#
-# print(data)
+import pandas as pd
+
+data = pd.DataFrame({'1': 97,
+                     '2': 86,
+                     '3': 0}, index=['Downloaded_subchunk']).T
+data.index.name = 'Chunk'
+
+print(data)
+
+print(data[data['Downloaded_subchunk'] == 0])
