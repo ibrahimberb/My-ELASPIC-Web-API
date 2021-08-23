59698b780b8980e9b0d185ebdf52b69c3c4a01d7
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-08-23 18:03:28 +0300

    Update basics.py

--- trash_scripts/basics.py
+++ trash_scripts/basics.py
@@ -1,10 +1,25 @@
+# Looks for each Record and checks if any entry contains default URL.
+# which should not be the case. If that entry is recorded, then it must have
+# the proper URL which can lead us to 'results' page.
+
 import pandas as pd
+from config import ELASPIC_MANY_URL
 
-data = pd.DataFrame({'1': 97,
-                     '2': 86,
-                     '3': 0}, index=['Downloaded_subchunk']).T
-data.index.name = 'Chunk'
 
-print(data)
+def url_fixer(tcga, chunk):
+    # it doesnt fix it right now.
 
-print(data[data['Downloaded_subchunk'] == 0])
+    record_path = rf'../Records/{tcga}/record_{tcga}_{chunk}.csv'
+
+    record_data = pd.read_csv(record_path)
+
+    data_url_default = record_data[record_data['ELASPIC_URL'] == ELASPIC_MANY_URL]
+    if not data_url_default.empty:
+        print(record_path)
+
+
+TCGA = 'OV'
+for chunk in range(1, 40):
+    url_fixer(TCGA, chunk)
+
+# ensurer(25, 1)
