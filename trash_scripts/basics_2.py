dea71dcc57ec30f78c1d508922c91c9be72be420
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-09-22 22:11:37 +0300

    Update basics_2.py

--- trash_scripts/basics_2.py
+++ trash_scripts/basics_2.py
@@ -1,27 +1,3 @@
-# # get number of active computations from each record.
-#
-# import pandas as pd
-# from config import ELASPIC_MANY_URL
-#
-#
-# def get_num_active_computations(tcga, chunk):
-#     record_path = rf'../Records/OV/record_{tcga}_{chunk}.csv'
-#
-#     record_data = pd.read_csv(record_path)
-#     num_active_computations = len(record_data[(record_data['UPLOADED_STATUS'] == 1) &
-#                                               (record_data['DOWNLOADED_STATUS'] == 0)])
-#
-#     print(f"TCGA: {TCGA} \t CHUNK: {chunk} \t NUM_ACTIVE_COMPUTATIONS: {num_active_computations}")
-#
-#
-# TCGA = 'OV'
-# for chunk in range(1, 40):
-#     get_num_active_computations(TCGA, chunk)
-#
-# # ensurer(25, 1)
+from utils.utils import read_chunks_to_run
 
-
-L = list(range(1, 128))
-exclude = [1, 2, 3, 4]
-L = [e for e in L if e not in exclude]
-print(L)
+print(read_chunks_to_run('coad'))
