3e6c508b86269a8882e91868d718301f8521655e
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-11-02 09:56:22 +0300

    Update summary.py

--- summary.py
+++ summary.py
@@ -11,15 +11,15 @@ from config import (
 
 import os
 import pandas as pd
+import os.path as op
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
-
 
 pd.set_option('display.max_rows', 500)
 
 
 def write_to_file(tcga: str, chunks: List[int]):
-    filepath = os.path.join(CHUNKS_TO_RUN_FOLDER_PATH, f'chunks_to_run_{tcga}.txt')
+    filepath = op.join(CHUNKS_TO_RUN_FOLDER_PATH, f'chunks_to_run_{tcga}.txt')
     with open(filepath, 'w') as file:
         for chunk in chunks[:-1]:  # Exclude the last chunk
             file.write(f"{chunk}\n")
@@ -27,25 +27,27 @@ def write_to_file(tcga: str, chunks: List[int]):
     print(f"Chunks for {tcga} are exported to file {filepath}")
 
 
-class TCGA:
-    def __init__(self, cohort_name):
-        self.cohort_name = str(cohort_name).upper()
-        self.input_files_path = os.path.join(INPUT_FILES_PATH, self.cohort_name)
+class Summary:
+    def __init__(self, tcga):
+        self.tcga = str(tcga).upper()
+        self.input_files_path = op.join(INPUT_FILES_PATH, self.tcga)
         self.chunks = self.get_input_files()
-        self.downloaded_files_path = os.path.join(ELASPIC_RESULTS_FOLDER_PATH, self.cohort_name)
+        self.downloaded_files_path = op.join(ELASPIC_RESULTS_FOLDER_PATH, self.tcga)
         self.chunks_to_num_downloaded_subchunks = self.get_chunks_to_num_downloaded_subchunks()
         self.chunks_to_num_active_computations = self.get_chunks_to_num_active_computations()
 
     def get_input_files(self):
         input_files = os.listdir(self.input_files_path)
+        logging.warning(input_files)
+        logging.warning(f"{self.input_files_path}")
         input_files_sorted = list(map(str, sorted(map(int, input_files))))
         return input_files_sorted
 
     def get_total_chunks(self):
-        return len(os.listdir(os.path.join(INPUT_FILES_PATH, self.cohort_name)))
+        return len(os.listdir(op.join(INPUT_FILES_PATH, self.tcga)))
 
     def get_downloaded_subchunks(self, chunk):
-        chunk_path = os.path.join(self.downloaded_files_path, chunk)
+        chunk_path = op.join(self.downloaded_files_path, chunk)
         try:
             return os.listdir(chunk_path)
         except FileNotFoundError:
@@ -54,8 +56,8 @@ class TCGA:
     def get_chunks_to_num_active_computations(self):
         chunks_to_num_active_computations = dict.fromkeys(map(str, self.chunks))
         for chunk in self.chunks:
-            record_filepath = rf'record_{self.cohort_name}_{chunk}.csv'
-            record_path = os.path.join(RECORDS_FOLDER_PATH, self.cohort_name, record_filepath)
+            record_filepath = rf'record_{self.tcga}_{chunk}.csv'
+            record_path = op.join(RECORDS_FOLDER_PATH, self.tcga, record_filepath)
             record_data = pd.read_csv(record_path)
             num_active_computations = len(record_data[(record_data['UPLOADED_STATUS'] == 1) &
                                                       (record_data['DOWNLOADED_STATUS'] == 0)])
@@ -84,9 +86,11 @@ class TCGA:
             print_table=True,
             subchunks_of=None,
             filter_=None,
-            threshold=90
+            threshold=90,
+            active_computations_only=False,
+            export=False
     ):
-        print(f"SUMMARY FOR TCGA   : \t {self.cohort_name}")
+        print(f"SUMMARY FOR TCGA   : \t {self.tcga}")
         table = self.get_summary_table()
         chunks_remaining = len(table[table['Downloaded_subchunk'] == 0])
         print(f"CHUNKS DOWNLOADED  : \t {len(self.chunks) - chunks_remaining} / {len(self.chunks)}")
@@ -105,7 +109,7 @@ class TCGA:
             print(table_print)
             downloaded = table_all['Downloaded_subchunk'].sum()
             total = len(self.chunks) * 100
-            print(f"Number of total downloaded subchunk files [{self.cohort_name}]: "
+            print(f"Number of total downloaded subchunk files [{self.tcga}]: "
                   f"{downloaded} of {total}"
                   f" ({round((downloaded / total) * 100, 2)} %)")
 
@@ -117,33 +121,74 @@ class TCGA:
                   f"{table_above_thr_chunks}")
             print(f"Chunks with downloaded sub-chunk below {threshold}:\n"
                   f"{table_below_thr_chunks}")
-            print(f"\n\t  - - -  (BELOW THRESHOLD {threshold}) - - - ")
             below_thr_ix = [e - 1 for e in table_below_thr_chunks]
             table_print_below_thr = table_all.iloc[below_thr_ix, :]
+
+            if active_computations_only:
+                print(f"\n\t  - - -  BELOW THRESHOLD {threshold} (Filtered for Active Computations) - - - ")
+                table_print_below_thr = table_print_below_thr[
+                    table_print_below_thr["NUM_ACTIVE_COMPUTATIONS"] > 0
+                    ]
+
+            else:
+                print(f"\n\t  - - -  (BELOW THRESHOLD {threshold}) - - - ")
+
             print(table_print_below_thr)
 
-            write_to_file(self.cohort_name, table_below_thr_chunks)
+            if export:
+                write_to_file(self.tcga, table_below_thr_chunks)
+
+    def bring_record(self, chunk_no):
+        record_path = op.join(
+            RECORDS_FOLDER_PATH, self.tcga, f"record_{self.tcga}_{chunk_no}.csv"
+        )
+        print(f"record_path: {record_path}")
+
+        record = pd.read_csv(record_path)
+
+        active_urls = record[
+            (record["UPLOADED_STATUS"] == 1) &
+            (record["DOWNLOADED_STATUS"] == 0)
+        ]["ELASPIC_URL"]
+        print(f"ACTIVE URLS: \n{active_urls}")
 
 
-# brca = TCGA('BRCA')
-# brca.get_summary(
+# coad = TCGA('COAD')
+# coad.get_summary(
 #     print_table=True,
-#     filter_=None,
-#     threshold=95
+#     threshold=90
 # )
 
-# ov = TCGA('OV')
-# ov.get_summary(
+# coad = TCGA('PAAD')
+# coad.get_summary(
 #     print_table=True,
-#     threshold=95
+#     threshold=90
 # )
 
-coad = TCGA('COAD')
-coad.get_summary(
+# don't worry about remaining active inputs if above 90 %
+# BRCA  :  5761 of  6100 (94.44 %)
+# OV    :  3654 of  3900 (93.69 %)
+
+# COAD  :  8580 of 12700 (67.56 %)
+
+# ESCA  :  1872 of 2000 (93.6 %)
+# HNSC  :  4741 of 5500 (86.2 %)
+
+#####################
+# # # COMPLETED # # #
+# tcga = "BRCA"
+# tcga = "OV"
+# tcga = "ESCA"
+#####################
+
+# tcga = "COAD"
+tcga = "HNSC"
+
+Summary(tcga).get_summary(
     print_table=True,
-    threshold=90
+    threshold=90,
+    export=False,
+    active_computations_only=True
 )
 
-# BRCA  :  5700 of  6100 (93.44 %)
-# OV    :  3640 of  3900 (93.33 %)
-# COAD  :  5877 of 12700 (46.28 %)
+# Summary(tcga).bring_record(20)
